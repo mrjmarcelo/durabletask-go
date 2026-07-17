7ffefddd760abe35d93746ec833bc097ff4b6560
Marcelo Rodrigues <Marcelo.Rodrigues2@dellteam.com>
2026-07-16 23:41:38 -0300

    remove order by parent instance id

--- backend/postgres/postgres.go
+++ backend/postgres/postgres.go
@@ -780,7 +780,7 @@ func (be *postgresBackend) GetOrchestrationWorkItem(ctx context.Context) (*backe
 				SELECT 1 FROM NewEvents E
 				WHERE E.InstanceID = I.InstanceID AND (E.VisibleTime IS NULL OR E.VisibleTime < $4)
 			)
-			ORDER BY I.ParentInstanceID, I.InstanceID, I.SequenceNumber ASC
+			ORDER BY I.InstanceID, I.SequenceNumber ASC
 			LIMIT 1
 			FOR UPDATE SKIP LOCKED
 		) RETURNING InstanceID`,
@@ -897,7 +897,7 @@ func (be *postgresBackend) GetOrchestrationWorkItems(ctx context.Context, batchS
 				SELECT 1 FROM NewEvents E
 				WHERE E.InstanceID = I.InstanceID AND (E.VisibleTime IS NULL OR E.VisibleTime < $4)
 			)
-			ORDER BY I.ParentInstanceID, I.InstanceID, I.SequenceNumber ASC
+			ORDER BY I.InstanceID, I.SequenceNumber ASC
 			LIMIT $5
 			FOR UPDATE SKIP LOCKED
 		) RETURNING InstanceID`,
